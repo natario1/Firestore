65c515fc15a81211cd9214b5b495b5ae7d7a55b1
Mattia Iavarone <mat.iavarone@gmail.com>
2018-10-24 16:20:09 +0200

    Add real cache and logs

--- firestore/src/main/kotlin/com/otaliastudios/firestore/Extensions.kt
+++ firestore/src/main/kotlin/com/otaliastudios/firestore/Extensions.kt
@@ -4,11 +4,25 @@ import com.google.firebase.firestore.DocumentSnapshot
 import kotlin.reflect.KClass
 
 @Suppress("UNCHECKED_CAST", "RedundantVisibilityModifier")
-public fun <T: FirestoreDocument> DocumentSnapshot.toFirestoreDocument(type: KClass<T>): T {
-    var result = FirestoreDocument.CACHE.get(reference.id) as? T
-    if (result == null) {
-        result = type.java.newInstance()!!
-        result.clearDirt() // Clear dirtyness from init().
+public fun <T: FirestoreDocument> DocumentSnapshot.toFirestoreDocument(type: KClass<T>, cache: Boolean = true): T {
+    val result = if (cache) {
+        val cached = FirestoreDocument.CACHE.get(reference.id) as? T
+        if (cached == null) {
+            FirestoreLogger.v("Id ${reference.id} asked for cache. Cache miss.")
+            val new = type.java.newInstance()
+            new.clearDirt() // Clear dirtyness from init().
+            FirestoreDocument.CACHE.put(reference.id, new)
+            new
+        } else {
+            FirestoreLogger.v("Id ${reference.id} asked for cache. Was found.")
+            cached
+        }
+    } else {
+        FirestoreLogger.v("Id ${reference.id} created with no cache.")
+        val new = type.java.newInstance()
+        new.clearDirt() // Clear dirtyness from init().
+        FirestoreDocument.CACHE.put(reference.id, new)
+        new
     }
     result.id = reference.id
     result.collection = reference.parent.path
@@ -17,8 +31,8 @@ public fun <T: FirestoreDocument> DocumentSnapshot.toFirestoreDocument(type: KCl
 }
 
 @Suppress("RedundantVisibilityModifier")
-public inline fun <reified T: FirestoreDocument> DocumentSnapshot.toFirestoreDocument(): T {
-    return toFirestoreDocument(T::class)
+public inline fun <reified T: FirestoreDocument> DocumentSnapshot.toFirestoreDocument(cache: Boolean = true): T {
+    return toFirestoreDocument(T::class, cache)
 }
 
 public fun <T: Any> firestoreListOf(vararg elements: T): FirestoreList<T> {
